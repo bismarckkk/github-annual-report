20666d08db4bad6a3846df5210b9c8f04d6fa5f6
bismarck <zuoqingyu@nuaa.edu.cn>
2022-12-30 13:45:04 +0800

    [feat] 修改到2022年

--- src/util.js
+++ src/util.js
@@ -1,35 +1,36 @@
-let client_id = '';
+let client_id = 'cea52b5b88e859d0a42f';
 if (process.env.NODE_ENV !== 'production') {
-    client_id = ''; // test id
+    client_id = 'cea52b5b88e859d0a42f'; // test id
 }
 const authorize_uri = 'https://github.com/login/oauth/authorize';
-const code_uri = 'http://192.168.1.10:8013/code2token';
-const redirect_uri = 'http://192.168.1.10:3000/oauth/redirect';
+const code_uri = 'http://127.0.0.1:8013/api/code2token';
+const redirect_uri = 'http://127.0.0.1:3000/oauth/redirect';
 
 export const getToken = async () => {
-    let token = '';
-    if (/\?code=(.+)&?/.test(window.location.search)) {
-        let code = window.location.search.match(/\?code=(.+)&?/)[1];
-        let res = await window.fetch(`${code_uri}?code=${code}`, {
-            headers: {
-                Accept: 'application/json'
-            },
-            mode: 'cors'
-        })
-        if (res) {
-            let r = await res.text()
-            if (r !== 'error') {
-                console.log(r)
-                set('token', r)
-                return r
-            } else {
-                alert('认证失败，请重新认证')
+    let token = await get('token');
+    if (token) {
+        console.log(`get token ${token}`)
+    } else {
+        if (/\?code=(.+)&?/.test(window.location.search)) {
+            let code = window.location.search.match(/\?code=(.+)&?/)[1];
+            let res = await window.fetch(`${code_uri}?code=${code}`, {
+                headers: {
+                    Accept: 'application/json'
+                },
+                mode: 'cors'
+            })
+            if (res) {
+                let r = await res.text()
+                if (r !== 'error') {
+                    console.log(r)
+                    set('token', r)
+                    return r
+                } else {
+                    alert('认证失败，请重新认证')
+                }
             }
         }
-    } else {
-        token = await get('token');
     }
-    console.log(`get token ${token}`)
     return token;
 };
 
@@ -69,8 +70,8 @@ export const get = key => {
 
 export const is2019 = created_at => {
     const time = +new Date(created_at);
-    const startTime = 1640966400000; // Tue Jan 01 2019 00:00:00 GMT+0800
-    const endTime = 1672502400000; // Tue Jan 01 2020 00:00:00 GMT+0800
+    const startTime = 1640966400000; // Tue Jan 01 2022 00:00:00 GMT+0800
+    const endTime = 1672502400000; // Tue Jan 01 2023 00:00:00 GMT+0800
     return time > startTime && time < endTime;
 };
 
